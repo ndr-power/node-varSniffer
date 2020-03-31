d5320fe5272acd8d2d9f0e4b6c0f848da3354c0b
ndr-power <ndrpowerr@gmail.com>
2020-03-31 12:50:43 +0300

    Update window.js

--- window.js
+++ window.js
@@ -11,3 +11,9 @@
 }
 let window1 = global.window
 let window = new Proxy(window1, proxyHandler)
+// list all new window vars
+Object.keys(window).filter(x => typeof(window[x]) !== 'function' &&
+  Object.entries(
+    Object.getOwnPropertyDescriptor(window, x)).filter(e =>
+      ['value', 'writable', 'enumerable', 'configurable'].includes(e[0]) && e[1]
+    ).length === 4)
